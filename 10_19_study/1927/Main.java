582e0fbd894b73cdc13e3a73bb835f7f9dcc8c16
takch02 <lim70kr38@gmail.com>
2025-11-01 23:20:36 +0900

    feat : 1927 해결

---
+++ 10_19_study/1927/Main.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * 구현방법 까먹어서 거의 보면서함.
+ * 이거 출력에 문제가 있는지 로직은 맞는데 자꾸 틀리다함 ㅅㅂ
+ */
+
+public class Main {
+    static ArrayList<Integer> heap = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        heap = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value > 0) push(value);
+            else if (value == 0 && heap.isEmpty()) {
+                sb.append("0").append("\n");
+            }
+            else {
+                sb.append(pop()).append("\n");
+            }
+        }
+        if (sb.isEmpty() == false) {
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+    public static void push(int value) {
+        heap.add(value);
+        int parent = (heap.size() - 1) / 2;
+        int child = heap.size() - 1;
+        while (parent >= 0) {
+            if (heap.get(child) < heap.get(parent)) {
+                int temp = heap.get(child);
+                heap.set(child, heap.get(parent));
+                heap.set(parent, temp);
+                child = parent;
+                parent = (child - 1) / 2;
+            }
+            else break;
+        }
+    }
+
+    public static int pop() {
+        int result = heap.get(0);
+        int lastValue = heap.remove(heap.size() - 1);
+
+        if (heap.isEmpty()) {
+            return result;
+        }
+        heap.set(0, lastValue);  // 마지막 요소를 맨 처음으로 가져온 후 아래로 내려가며 정렬
+
+        int parent = 0;
+        while (true) {
+            int left = parent * 2 + 1;
+            int right = parent * 2 + 2;
+            int smallest = parent;
+            if (left < heap.size() && heap.get(left) < heap.get(smallest)) {
+                smallest = left;
+            }
+            if (right < heap.size() && heap.get(right) < heap.get(smallest)) {
+                smallest = right;
+            }
+            if (smallest == parent) break;
+
+            int temp = heap.get(smallest);
+            heap.set(smallest, heap.get(parent));
+            heap.set(parent, temp);
+            parent = smallest;
+
+        }
+        return result;
+    }
+
+}
